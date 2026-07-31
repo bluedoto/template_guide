d840fb5dbb4cb71ec7fd08ea0ae374371e2ace71
Andy Suh <andysuh@Andys-MacBook-Pro-4.local>
2026-07-31 16:12:14 -0700

    Hide Streamlit footer real

--- invoice_generator.py
+++ invoice_generator.py
@@ -21,6 +21,9 @@ hide_streamlit_style = """
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
+div[data-testid="stToolbar"] {visibility: hidden; height: 0%; position: fixed;}
+div[data-testid="stDecoration"] {visibility: hidden; height: 0%; position: fixed;}
+div[data-testid="stStatusWidget"] {visibility: hidden; height: 0%; position: fixed;}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
